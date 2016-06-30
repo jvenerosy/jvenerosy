5bf5ab64839c537e34ba1f66341723d6fc4df6b1
jvenerosy <julien.venerosy@gmail.com>
2016-07-01 01:26:03 +0200

    trad v2

--- private/js/server.js
+++ private/js/server.js
@@ -2,27 +2,31 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 9740;
 
-app.use(express.static('public'));
+app.use('/static', express.static('public'));
 
 //routes
 app.get('/', function (req, res) {
-    res.render('pages/homepage.html.twig');
+    res.render('pages/fr/homepage.html.twig');
 });
 
-app.get('/portfolio', function (req, res) {
-    res.render('pages/portfolio.html.twig');
+app.get('/portfolio/', function (req, res) {
+    res.render('pages/fr/portfolio.html.twig');
 });
 
-app.get('/cv', function (req, res) {
-    res.render('pages/cv.html.twig');
+app.get('/cv/', function (req, res) {
+    res.render('pages/fr/cv.html.twig');
 });
 
-app.get('/resume', function (req, res) {
-    res.render('pages/cv-en.html.twig');
+app.get('/en', function (req, res) {
+    res.render('pages/en/homepage.html.twig');
 });
 
-app.use(function(req, res, next) {
-  res.render('pages/404.html.twig');
+app.get('/en/portfolio/', function (req, res) {
+    res.render('pages/en/portfolio.html.twig');
+});
+
+app.get('/en/cv/', function (req, res) {
+  res.render('pages/en/cv.html.twig');
 });
 
 app.listen(port);
